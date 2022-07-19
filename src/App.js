ae41279f03bb44e32d968891536ff2b228138cf6
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-07-19 18:51:37 +0300

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import "./App.css";
-import initializeFirebase from "./base/fb.config";
 import ChatPage from "./pages/ChatPage";
 import { getUserNickname } from "./services/local.service";
 import { MessagesContextProvider } from "./store/messages-context";
