db8d0e61372accccc2e8864e7bc42ddba32562d4
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-07-19 11:54:52 +0300

    Prepare message ui as a template

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -1,5 +1,13 @@
 import styles from "./MessageField.module.css";
+import sendLogo from "../assets/icons/send.png";
 
 export default function MessageField() {
-  return <div></div>;
+  return (
+    <div className={styles.messageField}>
+      <input type="text" placeholder="Write someting..." />
+      <button>
+        <img src={sendLogo} alt="send-logo" />
+      </button>
+    </div>
+  );
 }
