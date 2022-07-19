f9576bffa9aef50f7c28d549b18625e6d751a7ad
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-07-19 12:27:52 +0300

    initiliaze firebase in landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import "./App.css";
 import ChatPage from "./pages/ChatPage";
+import initializeFirebase from "./services/firabse.service";
 
 function App() {
+  useEffect(() => {
+    initializeFirebase();
+  }, []);
   return (
     <div className="App">
       <ChatPage />
