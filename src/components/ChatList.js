5dccbb8001f09bd83641576e03e641518a5a6252
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-07-19 18:21:36 +0300

    Retrieve messages data realtime and show in chat

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -1,43 +1,91 @@
+import { useEffect, useRef, useState } from "react";
 import styles from "./ChatList.module.css";
+import MessagesContext from "../store/messages-context";
+import { useContext } from "react";
 
-const DUMMY_CHATS = [
-  {
-    id: 1,
-    text: "test",
-    sender: "acac123",
-  },
-  {
-    id: 2,
-    text: "test2",
-    sender: "acac123",
-  },
-  {
-    id: 3,
-    text: "test3",
-    sender: "acac123",
-  },
-  {
-    id: 4,
-    text: "test4",
-    sender: "acac123",
-  },
-  {
-    id: 5,
-    text: "test5",
-    sender: "acac123",
-  },
-];
+import {
+  getDatabase,
+  ref,
+  onValue,
+  orderByChild,
+  query,
+} from "firebase/database";
+import initializeFirebase from "../base/fb.config";
 
 export default function ChatList() {
+  const [isLoading, setIsLoading] = useState(true);
+  // const [messages, setMessages] = useState([]);
+
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    setTimeout(() => {
+      if (messagesEndRef.current !== null)
+        messagesEndRef.current?.scrollIntoView({
+          behavior: "smooth",
+        });
+    }, 500);
+  };
+
+  let database = getDatabase(initializeFirebase());
+  const messagesContext = useContext(MessagesContext);
+
+  useEffect(() => {
+    getAllMessages();
+  }, []);
+
+  async function getAllMessages() {
+    const msgs = await getMessages();
+
+    // console.log(msgs);
+    // setMessages(msgs);
+    setIsLoading(false);
+    scrollToBottom();
+  }
+
+  async function getMessages() {
+    const starCountRef = query(
+      ref(database, "messages/"),
+      orderByChild("created_at")
+    );
+
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+
+      messagesContext.addMessage(convertResponseToArray(data));
+
+      scrollToBottom();
+    });
+
+    scrollToBottom();
+  }
+
+  function convertResponseToArray(data) {
+    var messages = [];
+
+    Object.values(data).forEach((item) => {
+      messages.push(JSON.parse(item));
+    });
+    // console.log(messages);
+    return messages;
+  }
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className={styles.chatList}>
-      {DUMMY_CHATS.map((message, index) => {
-        return (
-          <div key={index} className={styles.chatBox}>
-            <p>{message.text}</p>
-          </div>
-        );
-      })}
+      {messagesContext.messages === undefined
+        ? ""
+        : messagesContext.messages.map((message, index) => {
+            return (
+              <div key={index} className={styles.chatBox}>
+                <p>{message.text}</p>
+              </div>
+            );
+          })}
+      <div ref={messagesEndRef} style={{ float: "left", clear: "both" }}></div>
     </div>
   );
 }
